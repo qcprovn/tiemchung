9742ce89a9bdac8241c0835c24fc697fbb54f7dc
Nguyen Manh <manh.12g@gmail.com>
2021-11-04 10:54:40 +0700

    ok

---
+++ main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    show:false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadURL('https://tiemchungcovid19.moh.gov.vn/')
+//   mainWindow.loadURL("https://yte-nghean.vnpthis.vn/")
+  
+//   mainWindow.webContents.on('did-finish-load', function() {
+//     mainWindow.webContents.executeJavaScript(`document.getElementById("tendangnhap").value ="Nan_ttyt_quychau"`);
+//   });
+  mainWindow.once("ready-to-show", () => {
+    //   mainWindow.webContents.openDevTools();
+    mainWindow.maximize();
+    mainWindow.show();
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
---
+++ preload.js
@@ -0,0 +1,10 @@
+window.addEventListener("DOMContentLoaded", () => {
+  //   const replaceText = (selector, text) => {
+  //     const element = document.getElementById(selector);
+  //     if (element) element.value = text;
+  //   };
+  //   replaceText(`username`, `Nan_ttyt_quychau`);
+  //   replaceText(`password`, `Bvqc2016@`);
+  document.getElementById(`username`).value = "Nan_ttyt_quychau";
+  document.getElementById(`password`).value = "Bvqc2016@";
+});
